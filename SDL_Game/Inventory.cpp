aca9a65f9994e355ffa17df076a27f9d76a2ce83
BloodyAngel22 <NanoViper222@yandex.ru>
2023-04-07 22:15:31 +0300

    upd

--- SDL_Game/Inventory.cpp
+++ SDL_Game/Inventory.cpp
@@ -5,82 +5,110 @@
 
 weapons StWeapon;
 armors StArmors;
-extern bool lockItems;
-extern bool purchasedItem;
+bool lockItemsWeapon = false;
+bool lockItemsArmor = false;
+bool purchasedItem = true;
+int selectAnItem;
+int choiceItem;
+int removeItem;
+int choiceWeapon;
+
 void Invenory() {
 	int arrayWeapons[100] = { StWeapon.weapon1Damage, StWeapon.weapon2Damage, StWeapon.weapon3Damage, 
 		StWeapon.weapon4Damage
 	};
+	int idPastWeapon;
+	int idPastArmor;
 	int arrayArmors[100] = { StArmors.armor1Health, StArmors.armor2Health, StArmors.armor3Health, 
 		StArmors.armor4Health 
 	};
-	int choiceItem;
-	printf("1 - Weapons\n2 - Armors\n");
-	scanf_s("%d", &choiceItem);
-	if (choiceItem == 1) {
-		int choiceWeapon;
-		printf("Choose a weapon\n");
-		scanf_s("%d", &choiceWeapon);
-
-		#pragma region Weapon1
-		if (StWeapon.costWeapon1 == choiceWeapon and lockItems == false and purchasedItem == true) {
-			hero.Attack += StWeapon.weapon1Damage;
-			lockItems = true;
-		}
-		else if (StWeapon.costWeapon1 == choiceWeapon and lockItems == true and purchasedItem == true) {
-			printf("you removed the item\n");
-			hero.Attack -= StWeapon.weapon1Damage;
-		}
-		else if (StWeapon.costWeapon1 == choiceWeapon and purchasedItem == false) {
-			printf("This item has not been purchased\n");
-		}
-		#pragma endregion
-
-		#pragma region Weapon2
-		if (StWeapon.costWeapon2 == choiceWeapon and lockItems == false and purchasedItem == true) {
-			hero.Attack += StWeapon.weapon2Damage;
-			lockItems = true;
-		}
-		else if (StWeapon.costWeapon2 == choiceWeapon and lockItems == true and purchasedItem == true and useThisItem) {
-			printf("you removed the item\n");
-			hero.Attack -= StWeapon.weapon2Damage;
-		}
-		else if (StWeapon.costWeapon2 == choiceWeapon and purchasedItem == false) {
-			printf("This item has not been purchased\n");
-		}
-		#pragma endregion
-
-		#pragma region Weapon3
-		if (StWeapon.costWeapon3 == choiceWeapon and lockItems == false and purchasedItem == true) {
-			hero.Attack += StWeapon.weapon3Damage;
-			lockItems = true;
-		}
-		else if (StWeapon.costWeapon3 == choiceWeapon and lockItems == true and purchasedItem == true) {
-			printf("you removed the item\n");
-			hero.Attack -= StWeapon.weapon3Damage;
-		}
-		else if (StWeapon.costWeapon3 == choiceWeapon and purchasedItem == false) {
-			printf("This item has not been purchased\n");
-		}
+	bool Return_2 = true;
+	printf("do you want to select an item?\n0 - no\n1 - yes\n");
+	scanf_s("%d", &selectAnItem);
+	if (selectAnItem == 0) Return_2 = false;
+	while (Return_2) {
+		printf("1 - Weapons\n2 - Armors\n3 - exit\n");
+		scanf_s("%d", &choiceItem);
+		if (choiceItem == 1) {
+			bool Return = true;
+			while (Return) {
+				if (lockItemsWeapon == true) {
+					printf("Do you want to remove an item?\n0 - no\n1 - yes\n");
+					scanf_s("%d", &removeItem);
+					if (removeItem == 0) {
+						Return = false;
+					}
+				}
+				if (lockItemsWeapon == false) {
+					printf("Choose a weapon\n");
+					scanf_s("%d", &choiceWeapon);
+				}
+				for (int i = 0; i < sizeof(arrayWeapons) / sizeof(int); i++) { //Кол-во элементов массива
+#pragma region Weapon
+					if (i == choiceWeapon - 1 and lockItemsWeapon == false and purchasedItem == true) {
+						hero.Attack += arrayWeapons[i];
+						lockItemsWeapon = true;
+						idPastWeapon = i;
+						printf("Your damage %d\n", hero.Attack);
+					}
+					else if (i == choiceWeapon - 1 and lockItemsWeapon == true and purchasedItem == true and removeItem == 1) {
+						printf("you removed the item\n");
+						hero.Attack -= arrayWeapons[idPastWeapon];
+						printf("Your damage %d\n", hero.Attack);
+						lockItemsWeapon = false;
+					}
+					else if (i == choiceWeapon - 1 and purchasedItem == false) {
+						printf("This item has not been purchased\n");
+					}
+				}
+			}
 #pragma endregion
 
-		#pragma region Weapon4
-		if (StWeapon.costWeapon4 == choiceWeapon and lockItems == false and purchasedItem == true) {
-			hero.Attack += StWeapon.weapon4Damage;
-			lockItems = true;
 		}
-		else if (StWeapon.costWeapon4 == choiceWeapon and lockItems == true and purchasedItem == true) {
-			printf("you removed the item\n");
-			hero.Attack -= StWeapon.weapon4Damage;
-		}
-		else if (StWeapon.costWeapon4 == choiceWeapon and purchasedItem == false) {
-			printf("This item has not been purchased\n");
-		}
+		if (choiceItem == 2) {
+			int choiceArmor;
+			bool Return = true;
+			while (Return) {
+				if (lockItemsArmor == true) {
+					printf("Do you want to remove an item?\n0 - no\n1 - yes\n");
+					scanf_s("%d", &removeItem);
+					if (removeItem == 0) {
+						Return = false;
+					}
+				}
+				if (lockItemsArmor == false) {
+					printf("Choose a armor\n");
+					scanf_s("%d", &choiceArmor);
+				}
+				for (int i = 0; i < sizeof(arrayArmors) / sizeof(int); i++) { //Кол-во элементов массива
+#pragma region Armor
+					if (i == choiceArmor - 1 and lockItemsArmor == false and purchasedItem == true) {
+						hero.Health += arrayArmors[i];
+						lockItemsArmor = true;
+						idPastArmor = i;
+						printf("Your health %d\n", hero.Health);
+					}
+					else if (i == choiceArmor - 1 and lockItemsArmor == true and purchasedItem == true and removeItem == 1) {
+						printf("you removed the item\n");
+						hero.Health -= arrayArmors[idPastArmor];
+						printf("Your health %d\n", hero.Health);
+						lockItemsArmor = false;
+					}
+					else if (i == choiceArmor - 1 and purchasedItem == false) {
+						printf("This item has not been purchased\n");
+					}
+				}
+			}
 #pragma endregion
+		}
+		if (choiceItem == 3) Return_2 = false;
 	}
-	if (choiceItem == 2);
 }
 
 void Inventory_Battle() {
 
+}
+
+int main() {
+	Invenory();
 }
--- SDL_Game/Items.h
+++ SDL_Game/Items.h
@@ -1,19 +1,19 @@
 #pragma once
 
 struct weapons {
-	//damage weapon			//gold cost
-	int weapon1Damage = 10, costWeapon1 = 50;
-	int weapon2Damage = 15, costWeapon2 = 100;
-	int weapon3Damage = 20, costWeapon3 = 150;
-	int weapon4Damage = 25, costWeapon4 = 200;
+	//damage weapon			//gold cost			//is there an item
+	int weapon1Damage = 10, costWeapon1 = 50;	bool boughtWeapon1 = false;
+	int weapon2Damage = 15, costWeapon2 = 100;	bool boughtWeapon2 = false;
+	int weapon3Damage = 20, costWeapon3 = 150;	bool boughtWeapon3 = false;
+	int weapon4Damage = 25, costWeapon4 = 200;	bool boughtWeapon4 = false;
 };
 extern weapons StWeapon;
 
 struct armors {
-	//health weapon			//amount of armor		//goldcost
-	int armor1Health = 30, amountArmor1 = 10, costArmor1 = 50;
-	int armor2Health = 60, amountArmor2 = 20, costArmor2 = 100;
-	int armor3Health = 90, amountArmor3 = 30, costArmor3 = 150;
-	int armor4Health = 120, amountArmor4 = 40, costArmor4 = 200;
+	//health weapon			//amount of armor  //goldcost		//is there an item
+	int armor1Health = 30, amountArmor1 = 10, costArmor1 = 50;	bool boughtArmor1 = false;
+	int armor2Health = 60, amountArmor2 = 20, costArmor2 = 100;	bool boughtArmor2 = false;
+	int armor3Health = 90, amountArmor3 = 30, costArmor3 = 150;	bool boughtArmor3 = false;
+	int armor4Health = 120, amountArmor4 = 40, costArmor4 = 200;bool boughtArmor4 = false;
 };
 extern armors StArmors;
